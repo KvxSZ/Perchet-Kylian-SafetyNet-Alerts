3f63a75b566bc955babbbc03d71a0a870e1b6bd9
Kylian <kylian.openclassroom@gmail.com>
2024-01-02 00:17:02 +0100

    first commit

---
+++ SafetyNet-Alerts/src/test/java/fr/safetyNetAlerts/SafetyNet/Alerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,15 @@
+package fr.safetyNetAlerts.SafetyNet.Alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
